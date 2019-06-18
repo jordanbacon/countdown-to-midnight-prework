4c1926a37fe152225aff7389974238402fd08349
Jordan Bacon <jmbacon92@gmail.com>
2019-06-18 08:19:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,15 +2,15 @@
 
 def countdown(count)
   while count > 0
-  puts "#{number} SECOND(S)!"
+  puts "#{count} SECOND(S)!"
   count -= 1
 end
 "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(count)
-  while count >= 0
-  puts "#{number} SECOND(S)!"
+  while count > 0
+  puts "#{count} SECOND(S)!"
   sleep(1)
   count -= 1
 end
