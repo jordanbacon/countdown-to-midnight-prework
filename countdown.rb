d11b6c2321663684b16cf0e8db12c5ce697d8b18
Jordan Bacon <jmbacon92@gmail.com>
2019-06-18 08:14:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(count)
   puts "HAPPY NEW YEAR!"
 end
 end
+
+def countdown_with_sleep(count)
+  sleep(1)
+  while count >= 0
+  puts "#{number} SECOND(S)!"
+  count -= 1
+  puts "HAPPY NEW YEAR!"
+end
+end
